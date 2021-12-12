67f96ca3e034435034c17263bf3a4c33ffcd90ed
sastubblefield <stubblefield.sa@gmail.com>
2021-12-11 19:29:45 -0800

    remove code from appjs

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, Hello Shannon </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello, Hello Shannon </p>
     </div>
   );
 }
