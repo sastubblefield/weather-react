222b4e7e0ed200651e455e972047181d3a7ca1b7
sastubblefield <stubblefield.sa@gmail.com>
2021-12-12 08:37:51 -0800

    Added original code to appjs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, Hellow Shannon </p>
+        <h1>
+          <span className="temperature">78</span>
+          <span className="degrees">
+            <a className="fahrenheit-link" href="#">
+              {" "}
+              °F{" "}
+            </a>{" "}
+            |
+            <a className="celcius-link" href="#">
+              {" "}
+              °C{" "}
+            </a>
+          </span>
+        </h1>
+        <h2>
+          <div className="location">SAN DIEGO</div>
+          <div className="date">Friday 5:00</div>
+          <div className="other">
+            humidity: <span className="humidity">83</span>%<br />
+            wind: <span className="wind">2</span>mph
+            <br />
+          </div>
+          <div className="description">clear</div>
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
